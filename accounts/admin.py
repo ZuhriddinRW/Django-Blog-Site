6c54659e9578397180e3f71ab8cb0f955224572a
ZuhriddinRW <shodmonovzuhriddin2009@gmail.com>
2025-12-29 19:42:39 +0500

    formatted

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,8 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-class CustomUserAdmin(UserAdmin):
+
+class CustomUserAdmin ( UserAdmin ) :
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
@@ -12,20 +13,22 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ['is_staff', 'is_active']
 
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number', 'age')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {'fields' : ('username', 'password')}),
+        ('Personal info', {'fields' : ('first_name', 'last_name', 'email', 'phone_number', 'age')}),
+        ('Permissions', {'fields' : ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields' : ('last_login', 'date_joined')}),
     )
 
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'first_name', 'last_name', 'phone_number', 'password1', 'password2', 'is_staff', 'is_active')
+            'classes' : ('wide',),
+            'fields' : ('username', 'email', 'first_name', 'last_name', 'phone_number', 'password1', 'password2',
+                        'is_staff', 'is_active')
         }),
     )
 
     search_fields = ['username', 'email']
     ordering = ['username']
 
-admin.site.register(User, CustomUserAdmin)
+
+admin.site.register ( User, CustomUserAdmin )
--- accounts/models.py
+++ accounts/models.py
@@ -14,38 +14,38 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.db import models
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        if not username:
-            raise ValueError('Username is required!')
+class UserManager ( BaseUserManager ) :
+    def create_user(self, username, email=None, password=None, **extra_fields) :
+        if not username :
+            raise ValueError ( 'Username is required!' )
 
-        email = self.normalize_email(email) if email else None
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
+        email = self.normalize_email ( email ) if email else None
+        user = self.model ( username=username, email=email, **extra_fields )
+        user.set_password ( password )
+        user.save ( using=self._db )
         return user
 
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, username, email=None, password=None, **extra_fields) :
+        extra_fields.setdefault ( 'is_staff', True )
+        extra_fields.setdefault ( 'is_superuser', True )
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError("Superuser's property is_staff must be True")
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError("Superuser's property is_superuser must be True")
+        if extra_fields.get ( 'is_staff' ) is not True :
+            raise ValueError ( "Superuser's property is_staff must be True" )
+        if extra_fields.get ( 'is_superuser' ) is not True :
+            raise ValueError ( "Superuser's property is_superuser must be True" )
 
-        return self.create_user(username, email, password, **extra_fields)
+        return self.create_user ( username, email, password, **extra_fields )
 
 
-class User(AbstractUser, PermissionsMixin):
+class User ( AbstractUser, PermissionsMixin ) :
     email = models.EmailField ( blank=True, null=True )
-    age = models.PositiveIntegerField(null=True, blank=True)
-    phone_number = models.CharField(unique=True, max_length=15, blank=True, null=True)
+    age = models.PositiveIntegerField ( null=True, blank=True )
+    phone_number = models.CharField ( unique=True, max_length=15, blank=True, null=True )
 
-    objects = UserManager()
+    objects = UserManager ()
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
 
-    def __str__(self):
+    def __str__(self) :
         return self.username
