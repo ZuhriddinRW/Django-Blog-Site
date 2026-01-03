431f65c10df9ddfcfa3d3f7e66944c498486581f
ZuhriddinRW <shodmonovzuhriddin2009@gmail.com>
2026-01-03 10:33:28 +0500

    formatted

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,18 +8,17 @@ urlpatterns = [
     path ( 'signin/', sign_in, name='signin' ),
     path ( 'signout/', sign_out, name='signout' ),
 
-    # Password change (requires login)
-    path(
+    path (
         'password/change/',
-        auth_views.PasswordChangeView.as_view(
+        auth_views.PasswordChangeView.as_view (
             template_name='Authentication/password_change.html',
             success_url='/password/change/done/',
         ),
         name='password_change',
     ),
-    path(
+    path (
         'password/change/done/',
-        auth_views.PasswordChangeDoneView.as_view(
+        auth_views.PasswordChangeDoneView.as_view (
             template_name='Authentication/password_change_done.html'
         ),
         name='password_change_done',
